d0f601442e1a95dd6504dc89d9acc7ba7fa8c356
yfqiao <qiao.yif@northeastern.edu>
2022-07-18 22:04:45 -0700

    update lint

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+# from django.urls import path, include
+# from django.conf.urls.static import static
+# from django.conf import settings
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
