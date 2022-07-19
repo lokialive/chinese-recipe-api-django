a35671a605e48082ee382f2572784ddae3dce0e9
yfqiao <qiao.yif@northeastern.edu>
2022-07-18 20:58:14 -0700

    Finish user feature

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -25,27 +25,30 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-    # def test_new_user_email_normalized(self):
-    #     """Test the email for a new user is normalized"""
-    #     email = 'test@LONDONAPPDEV.COM'
-    #     user = get_user_model().objects.create_user(email, 'test123')
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com']
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'testpass123')
+            self.assertEqual(user.email, expected)
 
-    #     self.assertEqual(user.email, email.lower())
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
 
-    # def test_new_user_invalid_email(self):
-    #     """Test creating user with no email raises error"""
-    #     with self.assertRaises(ValueError):
-    #         get_user_model().objects.create_user(None, 'test123')
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'testpass123'
+        )
 
-    # def test_create_new_superuser(self):
-    #     """Test creating a new superuser"""
-    #     user = get_user_model().objects.create_superuser(
-    #         'test@londonappdev.com',
-    #         'test123'
-    #     )
-
-    #     self.assertTrue(user.is_superuser)
-    #     self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
 
     # def test_tag_str(self):
     #     """Test the tag string representation"""
